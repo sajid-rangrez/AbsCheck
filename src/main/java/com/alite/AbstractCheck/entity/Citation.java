b35a7251522da09709e54f96eaf8772865dddc11
sajid <sajidhussain3327@gmail.com>
2024-01-18 11:39:02 +0530

    now compatible with java 1.8

--- src/main/java/com/alite/AbstractCheck/entity/Citation.java
+++ src/main/java/com/alite/AbstractCheck/entity/Citation.java
@@ -1,8 +1,8 @@
 package com.alite.AbstractCheck.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name = "lit_citation")
 public class Citation {
