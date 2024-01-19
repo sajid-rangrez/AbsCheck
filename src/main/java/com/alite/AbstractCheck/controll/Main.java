964b7ee197ce71d467e3c346d3517a351b489dfe
sajid <sajidhussain3327@gmail.com>
2024-01-19 12:15:26 +0530

    no need to use browser

--- src/main/java/com/alite/AbstractCheck/controll/Main.java
+++ src/main/java/com/alite/AbstractCheck/controll/Main.java
@@ -5,24 +5,46 @@ import com.alite.AbstractCheck.entity.Citation;
 import com.alite.AbstractCheck.service.AbsTextService;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 
-@RestController
-public class Main {
+//@RestController
+@Component
+public class Main implements CommandLineRunner {
     @Autowired
     private MyRepository repository;
     @Autowired
     private AbsTextService service;
-    @GetMapping("/start")
-    public void findAll() throws IOException {
-        service.getData();
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Select:");
+        System.out.println("To Compare type: 1");
+        System.out.println("To Generate Only CSV type: 2");
+        System.out.println("To Exit Type: 3");
+        int t  = scan.nextInt();
+        if(t == 1) service.getData();
+        else if(t == 2) service.getDataToCSV();
+        else if(t == 3) System.out.println("End");
+        else System.out.println("Wrong Input");
+        System.out.println("Program Ended!");
     }
-    @GetMapping("/csvPrint")
-    public void generateCSV(){
-        service.getDataToCSV();
-    };
+
+
+//    @GetMapping("/start")
+//    public void findAll() throws IOException {
+//        service.getData();
+//    }
+//    @GetMapping("/csvPrint")
+//    public void generateCSV(){
+//        service.getDataToCSV();
+//    };
 }
